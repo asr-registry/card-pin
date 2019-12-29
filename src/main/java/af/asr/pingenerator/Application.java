b528710f8fc14443145c7838c931e6ecef8b2861
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-29 13:38:53 +0430

    card processor refactor

--- src/main/java/af/asr/pingenerator/Application.java
+++ src/main/java/af/asr/pingenerator/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PingeneratorApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PingeneratorApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
