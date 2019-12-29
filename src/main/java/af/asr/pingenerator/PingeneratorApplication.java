0996c62c67baa85f4b90cb38158639d571052d2c
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-29 10:37:50 +0430

    init stp

---
+++ src/main/java/af/asr/pingenerator/PingeneratorApplication.java
@@ -0,0 +1,13 @@
+package af.asr.pingenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingeneratorApplication.class, args);
+	}
+
+}
